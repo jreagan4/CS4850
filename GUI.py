fbad3840d1769d35f113e7bd681b0bf92fc8995c
jreagan4 <jreagan1591@gmail.com>
2016-11-03 16:03:50 -0400

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+#set up root window
+root = Tk()
+#set dimensions of the window
+root.title("Project Yodel")
+root.geometry("800x600")
+
+
+#start main loop
+root.mainloop()
