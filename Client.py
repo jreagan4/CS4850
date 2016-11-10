69b654d94cfa3e189e1a31ca92e8b4b0bd34ac79
jreagan4 <jreagan1591@gmail.com>
2016-11-10 14:15:04 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -12,16 +12,19 @@ def setPort(port):
        TCP_PORT = port
         
 def startConn(ip, port):
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # this is magic socket library stuff, just go with it
-    soc.connect((TCP_IP, TCP_PORT)) # connect to determined port and IP with our new socket object made from magic
+       soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # this is magic socket library stuff, just go with it
+       soc.connect((TCP_IP, TCP_PORT)) # connect to determined port and IP with our new socket object made from magic
 
-    while 1:
-    MESSAGE = input("Enter message: ")
-    soc.send(MESSAGE.encode('utf-8'))
-    """
-    .encode() is important here, as sockets can only deal with bit-objects
-    utf-8 makes the data decode as a string object
-    """
+       while 1:
+       MESSAGE = input("Enter message: ")
+       soc.send(MESSAGE.encode('utf-8'))
+       """
+       .encode() is important here, as sockets can only deal with bit-objects
+       utf-8 makes the data decode as a string object
+       """
+       data = soc.recv(BUFF_SIZE)
+       print(data.decode('utf-8'))
+    
 
 startConn(TCP_IP, TCP_PORT)
 
