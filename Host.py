993baec31c22a6ef4c7d118f91eb42878d22b15a
jreagan4 <jreagan1591@gmail.com>
2016-11-14 16:33:25 -0500

    Update to UDP broadcast
    functions mainly as a listener, should probably modify to be the one handing out memberships to mcast groups.

--- Host.py
+++ Host.py
@@ -1,35 +1,29 @@
 import socket
-from _thread import start_new_thread
+import struct
 
 """
 packet loss against host, if host < threshold test all connections for better stability
 if too many host swaps occur, stop host swapping for x amount of time
 """
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5555
+MCAST_GRP = '224.2.34.56'
+SERV_ADDR = ('', 5555)
+
 BUFF_SIZE = 1024
 
 def start_soc():
-        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        soc.bind((TCP_IP, TCP_PORT))
-        soc.listen(10)
+        soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        soc.bind(SERV_ADDR)
+        GRP = socket.inet_aton(MCAST_GRP)
+        MREQ = struct.pack('4sL', GRP, socket.INADDR_ANY)
+        soc.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, MREQ)
         return soc
-
-def socket_handler(client):
-    while True:
-        data = client.recv(BUFF_SIZE)
-        chost, cport = client.getnameinfo()
-        if data :
-            print("received data: ", data.decode('utf-8'))
-            client.send(data)
 
 def run():
     soc = start_soc()
     while 1:
-        client, addr = soc.accept()
-        print("connection from ", addr)
-        start_new_thread(socket_handler, (client,))
+        data, addr = soc.recvfrom(BUFF_SIZE)
+        print("Data from ", addr)
 
 
 if __name__ == '__main__':
