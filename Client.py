81e1ec9d4e9cf56e39a17d41406cc82dbe4fabbd
jreagan4 <jreagan1591@gmail.com>
2016-11-14 17:33:12 -0500

    Update Client.py
    Update for things to do stuff better

--- Client.py
+++ Client.py
@@ -5,29 +5,37 @@ MCAST_GROUP = ('224.2.34.56', 5555)
 BUFF_SIZE = 1024
 MESSAGE = "This did not work"
 
-    
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # this is magic socket library stuff, just go with it\
+
+def sendMessage():
+    MESSAGE = input("Enter message: ")
+    if MESSAGE is '':
+       sendMessage()
+    else:
+        soc.sendto(MESSAGE.encode('utf-8'), MCAST_GROUP)
+
+def recieveMessage():
+    data, addr = soc.recvfrom(BUFF_SIZE)
+    print(data.decode('utf-8'))
+
 def startConn():
-       soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # this is magic socket library stuff, just go with it
-       soc.settimeout(0.3) # sets timeout so we don't block forever
-       ttl = struct.pack('b', 1) # sets time to live to 1 hop for local testing, need to change this later, format for windows to not be dumb
-       soc.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl) # set socket options, also magic
+    soc.settimeout(0.3)  # sets timeout so we don't block forever
+    ttl = struct.pack('b',
+                      1)  # sets time to live to 1 hop for local testing, need to change this later, format for windows to not be dumb
+    soc.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)  # set socket options, also magic
 
-        while 1:
-              MESSAGE = input("Enter message: ")
-              soc.sendto(MESSAGE.encode('utf-8'), MCAST_GROUP)
-              """
-              .encode() is important here, as sockets can only deal with bit-objects
-              utf-8 makes the data decode as a string object
-              """
-        try:
-              data, addr = soc.recvfrom(BUFF_SIZE)
-              print("%s: %s" % (addr, data.decode('utf-8')))
 
-        except socket.timeout:
-            # do other stuff
+    while 1:
+        sendMessage()
+        recieveMessage()
+        """
+        .encode() is important here, as sockets can only deal with bit-objects
+        utf-8 makes the data decode as a string object
+        """
+
 try:
-       startConn()
+    startConn()
 
 finally:
-       print("disconnecting")
-       soc.close()
+    print("disconnecting")
+    soc.close()
