fea7fdece7c17fda658f721dc0b7c16cd805293d
jreagan4 <jreagan1591@gmail.com>
2016-11-14 20:46:53 -0500

    Update Client.py
    Better stablility

--- Client.py
+++ Client.py
@@ -1,41 +1,55 @@
 import socket
 import struct
+import threading
 
-MCAST_GROUP = ('224.2.34.56', 5555)
+MCAST_ADDR = '224.2.34.56'
+MCAST_GRP = ('224.2.34.56', 5555)
+SERV_ADDR = ('', 5555)
 BUFF_SIZE = 1024
 MESSAGE = "This did not work"
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # this is magic socket library stuff, just go with it\
 
-def sendMessage():
+def start_soc():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    soc.bind(SERV_ADDR)
+    GRP = socket.inet_aton(MCAST_ADDR)
+    MREQ = struct.pack('4sL', GRP, socket.INADDR_ANY)
+    soc.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, MREQ)
+    return soc
+
+def sendMessage(soc):
     MESSAGE = input("Enter message: ")
     if MESSAGE is '':
-       sendMessage()
+        recieveMessage(soc)
     else:
-        soc.sendto(MESSAGE.encode('utf-8'), MCAST_GROUP)
+        soc.sendto(MESSAGE.encode('utf-8'), MCAST_GRP)
 
-def recieveMessage():
-    data, addr = soc.recvfrom(BUFF_SIZE)
-    print(data.decode('utf-8'))
+def recieveMessage(soc):
+    while 1:
+        try:
+            data, addr = soc.recvfrom(BUFF_SIZE)
+            while data:
+                if not data:
+                    break
+                print(data.decode('utf-8'))
+                data, addr = soc.recvfrom(BUFF_SIZE)
+        except socket.timeout:
+            sendMessage(soc)
 
-def startConn():
+def startConn(soc):
     soc.settimeout(0.3)  # sets timeout so we don't block forever
     ttl = struct.pack('b',
                       1)  # sets time to live to 1 hop for local testing, need to change this later, format for windows to not be dumb
     soc.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)  # set socket options, also magic
 
 
-    while 1:
-        sendMessage()
-        recieveMessage()
-        """
-        .encode() is important here, as sockets can only deal with bit-objects
-        utf-8 makes the data decode as a string object
-        """
+    threading.Thread(target=recieveMessage(soc), args=[soc]).start()
+
 
 try:
-    startConn()
+    soc = start_soc()
+    startConn(soc)
+
 
 finally:
     print("disconnecting")
-    soc.close()
