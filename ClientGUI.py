94e3096ec23c0aaf9e063aa03aa66e73b6aebdb2
sjolly889 <sjolly889@gmail.com>
2016-12-01 01:45:35 -0500

    Update ClientGUI.py

--- ClientGUI.py
+++ ClientGUI.py
@@ -3,7 +3,10 @@ import socket
 import struct
 import threading
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QStyleFactory
+from PyQt5.Qt import QStringListModel
+#from PyQt5.QtCore import *
+#from PyQt5.QtGui import *
 
 MCAST_ADDR = '224.2.34.56'
 MCAST_GRP = ('224.2.34.56', 5555)
@@ -11,7 +14,7 @@ SERV_ADDR = ('', 5555)
 BUFF_SIZE = 75
 MESSAGE = "This did not work"
 
-qtCreatorFile = "GUI.ui" # Enter file here.
+qtCreatorFile = "GUI_V3.ui" # Enter file here.
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 soc = socket.socket                
@@ -67,15 +70,30 @@ class MyApp(QMainWindow):
         Ui_MainWindow.__init__(self)
         self.ui.setupUi(self)
         self.ui.sendButton.clicked.connect(self.handleSendButton)
+        self.ui.addToCallButton.clicked.connect(self.handleAddToCallButton)
+        self.ui.removeFromCallButton.clicked.connect(self.handleRemoveFromCallButton)
         
-    def handleSendButton(self):
+    def updateText(self, text):
+        self.ui.chatBox.addItem(text)
+            
+    def handleSendButton(self):        
         MESSAGE = self.ui.chatInputBox.text()
-        self.ui.chatBox.addItem((MESSAGE))
-        self.ui.chatInputBox.select_all()
+        if MESSAGE is not '':
+            self.ui.chatBox.addItem('Peter: ' +MESSAGE)
+        self.ui.chatBox.scrollToBottom()
         self.ui.chatInputBox.clear()
+        
+    def handleAddToCallButton(self):          
+        self.ui.currentCallList.addItem(self.ui.contactsList.currentItem())
+        self.ui.contactsList.removeItemWidget(self.ui.contactsList.currentItem())
+        
+    def handleRemoveFromCallButton(self):
+        self.ui.contactList.addItem(self.ui.currentCallList.currentItem())
+        self.ui.currentCallList.removeItemWidget(self.ui.currentCallList.currentItem())
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
     window = MyApp()
     window.show()
     sys.exit(app.exec_())
